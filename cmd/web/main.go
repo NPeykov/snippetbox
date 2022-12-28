765b87a8d68e6c1f52523b2665be0ac412b41cab
peykov <peykovn@gmail.com>
2022-12-28 17:16:28 -0300

    refactor: routes, logs, server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,18 +4,32 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+    infoLog *log.Logger
+    errorLog *log.Logger
+}
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP application port")
     flag.Parse()
-	mux := http.NewServeMux()
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Printf("Starting server on port %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate | log.Ltime)
+    errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate | log.Ltime | log.Lshortfile)
+    app := &application{
+        infoLog: infoLog, 
+        errorLog: errorLog,
+    }
+
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: app.routes(),
+    }
+
+	infoLog.Printf("Starting server on port %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
