412cae21b61f4380fbb8e5cf83d22cc43d4be5a0
Hugo Peykovich <peykovn@gmail.com>
2023-01-15 18:22:34 -0300

    user signup implementation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,6 +18,13 @@ type snippetCreateForm struct {
     validator.Validator `form:"-"`
 }
 
+type userSignupForm struct {
+    Name     string     `form:"name"`
+    Email    string     `form:"email"`
+    Password string     `form:"password"`
+    validator.Validator `form:"-"`
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     snippets, err := app.snippets.Latest()
 
@@ -112,11 +119,51 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("signup"))
+    data := app.newTemplateData(r)
+    data.Form = userSignupForm {}
+    app.render(w, http.StatusOK, "signup.tmpl.html", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("signup post"))
+    var form userSignupForm
+
+    err := app.decodePostForm(r, &form)
+
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be empty")
+    form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be empty")
+    form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be empty")
+    form.CheckField(validator.MinChars(form.Password, 8), "password", "The password must have more than 8 characters")
+    form.CheckField(validator.Matches(form.Email, validator.EmailRegex), "email", "This email is not valid")
+
+    if !form.Valid() {
+        data := app.newTemplateData(r)
+        data.Form = form
+        app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+        return
+    }
+
+    err = app.users.Insert(form.Name, form.Email, form.Password)
+
+    if err != nil {
+        if errors.Is(err, models.ErrDuplicateEmail) {
+            form.AddFieldError("email", "Email is alredy in use")
+
+            data := app.newTemplateData(r)
+            data.Form = form
+            app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+            return
+        }
+        app.serverError(w, err)
+        return
+    }
+
+    app.sessionManager.Put(r.Context(), "flash", "Successfully signedup. Now you can log in.")
+    http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogout(w http.ResponseWriter, r *http.Request) {
