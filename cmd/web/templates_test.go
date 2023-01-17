093aee020bb2573cf824b49b83a2d380c4557549
Hugo Peykovich <peykovn@gmail.com>
2023-01-17 17:03:33 -0300

    testing humanDate()

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "testing"
+    "time"
+)
+
+func TestHumanData(t *testing.T) {
+    tests := []struct {
+        name string
+        tm time.Time
+        expect string
+    }{
+        {
+            name : "UTC",
+            tm   : time.Date(2023, 1, 17, 10, 15, 0, 0, time.UTC),
+            expect : "17 Jan 2023 at 10:15",
+        },
+        {
+            name : "Empty",
+            tm : time.Time{},
+            expect :  "",
+        },
+        {
+            name : "CET",
+            tm : time.Date(2023, 1, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+            expect : "17 Jan 2023 at 09:15",
+        },
+    }
+
+    for _, test := range tests {
+        t.Run(test.name, func(t *testing.T) {
+            got := humanDate(test.tm)
+            if got != test.expect {
+                t.Errorf("got %q; want %q", got, test.expect)
+            }
+        })
+    }
+}
