eab859f1f153267ee6b8e289d8a4e47bb875ef5c
Hugo Peykovich <peykovn@gmail.com>
2023-01-17 20:05:23 -0300

    test utils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/NPeykov/snippetbox/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-    recorder := httptest.NewRecorder()
+    app := newTestApplication()
+    ts := newTestServer(t, app.routes())
+    defer ts.Close()
 
-    req, err := http.NewRequest(http.MethodGet, "/", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+    code, _, body := ts.get(t, "/ping")
 
-    ping(recorder, req)
-
-    rs := recorder.Result()
-
-    assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-    defer rs.Body.Close()
-    body, err := io.ReadAll(rs.Body)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    bytes.TrimSpace(body)
-    assert.Equal(t, string(body), "OK")
+    assert.Equal(t, code, http.StatusOK)
+    assert.Equal(t, body, "OK")
 }
